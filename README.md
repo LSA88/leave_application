b0e01d70ca59dfd027f8d99ab108539d00fb6921
LSA88 <lovervz@naver.com>
2022-11-22 16:36:34 +0900

    결재라인 결재방법 모달

--- README.md
+++
@@ -1,2 +0,0 @@
-# leave_application
- 
---
+++ js/modal.js
@@ -0,0 +1,28 @@
+var modal = {
+  open: function () {
+    $("#modal-wrapper").show();
+  },
+  close: function () {
+    $("#modal-wrapper").hide();
+  },
+};
+
+$(document)
+  .on("click", "#modal-overlay", function () {
+    window.history.back();
+  })
+  .on("click", ".approval__line__method", function () {
+    window.history.pushState({}, "modal", "/modal");
+    modal.open();
+  })
+  .on("click", ".search__btn", function () {
+    window.history.pushState({}, "modal", "/modal");
+    modal.open();
+  });
+
+window.onpopstate = history.onpushstate = function (e) {
+  if (window.location.href.split("/").pop().indexOf("modal") === -1) {
+    // 마지막 segment가 cards라면 모달이 아닌 리스트인 상태이어야한다.
+    modal.close(); // 현재의 모달을 닫는다.
+  }
+};
